7656c93687745d6f99121cf419a2036018037e35
Merijn <merijn2@gmail.com>
2021-05-18 10:18:31 +0200

    add return to subtract and multiply)

--- calc.py
+++ calc.py
@@ -1,11 +1,15 @@
 def add(x,y):
     pass
+
 def subtract(x,y):
-    pass
+    return(x-y)
+
 def multiply(x,y):
-    pass
+    return (x*y)
+
 def divide(x,y):
     pass
+
 def square(x):
     pass
   
